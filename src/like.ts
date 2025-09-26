dad82289563d7d63ce26951fc8484b0d52f4a657
Lee Seunghyeon <lsh09130@gmail.com>
2025-09-27 01:26:20 +0900

    Add memory storage for likes

--- src/like.ts
+++ src/like.ts
@@ -1,8 +1,14 @@
+const likes = new Set<string>(); // 일단은 휘발성, 나중에 필요하면 .json 등으로 저장할 수 있음
+
 export function initLikeFeature(section: HTMLElement): void {
     section.querySelectorAll<HTMLButtonElement>(".like-btn").forEach(btn => {
         btn.addEventListener("click", e => {
-        e.preventDefault();
-        btn.classList.toggle("liked");
+            e.preventDefault();
+            const card = (e.target as HTMLElement).closest('.card') as HTMLElement;
+            const id = card.dataset.id!;
+            btn.classList.toggle("liked");
+            if (btn.classList.contains('liked')) likes.add(id);
+            else likes.delete(id);
         });
     });
 }
